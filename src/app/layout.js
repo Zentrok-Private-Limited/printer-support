9a60f8fd592005ee79723593e8f50cfd5d0debdb
Komal <ks989156@gmail.com>
2025-11-05 11:39:05 +0530

    feat: add Google Tag Manager scripts for enhanced tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Mulish } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const mulish = Mulish({
   variable: "--font-mulish",
@@ -14,6 +15,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html name="viewport" content="width=device-width, initial-scale=1" lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17693950882"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17693950882');
+          `}
+        </Script>
+      </head>
+
       <body
         className={`${mulish.variable}} antialiased`}
       >
